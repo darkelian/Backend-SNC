be413a10c28a656bb26c5562026b14e632e877ce
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2025-03-12 20:28:16 -0500

    feat: :construction: Estructura inicial del proyecto

---
+++ backend_snc/src/test/java/com/snc/backend/BackendSncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
