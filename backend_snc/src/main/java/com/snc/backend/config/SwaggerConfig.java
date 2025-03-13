7c5cb32da0113e64b25ebda9af889c142c42d0b9
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2025-03-12 21:55:10 -0500

    chore: :construction: Configuración inicial con swagger

---
+++ backend_snc/src/main/java/com/snc/backend/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.snc.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI caseOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Servicios Backend de SNC")
+                        .description("Documentación detallada de los servicios y esquemas para los modulos de SNC")
+                        .version("1.0.1"))
+                .addTagsItem(new Tag().name("Administrador").description("Operaciones del modulo de administrador"));
+    }
+}
